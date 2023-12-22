4d09f9537508df972fe61ae25d93b0406b8f7566
yj94 <2991645986@qq.com>
2023-12-22 22:39:11 +0800

    Week1的案例代码
    DLL调用
    DLL直接注入
    DLL远线程注入
    DLL反射注入
    方法的实现
    以及测试dll代码

---
+++ Week1/Dll_TEST/dllmain.cpp
@@ -0,0 +1,61 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+#include <Windows.h>
+extern "C" __declspec(dllexport) VOID ShowMessage(const char* message);
+extern "C" __declspec(dllexport) INT main();
+
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        OutputDebugString(L"DLL Inject Sucess");
+        MessageBox(NULL, L"dll sucess", L"dll sucess", MB_OK);
+        break;//加载DLL时运行d
+    case DLL_THREAD_ATTACH:
+        OutputDebugString(L"DLL free Sucess");
+        MessageBox(NULL, L"dll free", L"dll free", MB_OK);
+        break;//加载DLL时运行d
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+LRESULT CALLBACK KeyboardProc(
+    int code,
+    WPARAM wParam,
+    LPARAM lParam
+)
+{
+    if (code == HC_ACTION)
+    {
+        if (wParam == VK_INSERT && (lParam & (1 << 30)))
+            MessageBoxA(NULL, "您按下了HOME键", "HOME", MB_OK);
+    }
+
+    return  CallNextHookEx(0, code, wParam, lParam);
+
+}
+
+extern "C" _declspec(dllexport)  HOOKPROC getDllPoint(void)
+{
+    return KeyboardProc;
+}
+void ShowMessage(const char* message)
+{
+    MessageBoxA(NULL, message, "DLL Message", MB_OK);
+}
+int main() {
+    MessageBoxA(NULL, "调用dll成功", "DLL Message", MB_OK);
+    int wmId = 0;
+    if (wmId == 1002) {
+        MessageBoxA(NULL, "按钮被劫持了！", "提示", MB_ICONINFORMATION);
+    }
+    return 0;
+}
---
+++ Week1/Win32_From/main.cpp
@@ -0,0 +1,162 @@
+// HelloWindowsDesktop.cpp
+// compile with: /D_UNICODE /DUNICODE /DWIN32 /D_WINDOWS /c
+
+#include <windows.h>
+#include <stdlib.h>
+#include <string.h>
+#include <tchar.h>
+#include <iostream>
+#include "header.h"
+// Global variables
+
+// The main window class name.
+static TCHAR szWindowClass[] = _T("DesktopApp");
+
+// The string that appears in the application's title bar.
+static TCHAR szTitle[] = _T("Win32_From Test");
+
+// Stored instance handle for use in Win32 API calls such as FindResource
+HINSTANCE hInst;
+
+// Forward declarations of functions included in this code module:
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPSTR     lpCmdLine,
+    _In_ int       nCmdShow
+)
+{
+    WNDCLASSEX wcex;
+    //dllFuncs();
+    //TestDllInject();
+    RemoteDllInject();
+    //refInject();
+    openDebugPri();
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+
+    if (!RegisterClassEx(&wcex))
+    {
+        MessageBox(NULL,
+            _T("注册窗口类失败"),
+            (NULL),
+            NULL);
+
+        return 1;
+    }
+
+    // Store instance handle in our global variable
+    hInst = hInstance;
+
+    // The parameters to CreateWindowEx explained:
+    // WS_EX_OVERLAPPEDWINDOW : An optional extended window style.
+    // szWindowClass: the name of the application
+    // szTitle: the text that appears in the title bar
+    // WS_OVERLAPPEDWINDOW: the type of window to create
+    // CW_USEDEFAULT, CW_USEDEFAULT: initial position (x, y)
+    // 500, 100: initial size (width, length)
+    // NULL: the parent of this window
+    // NULL: this application does not have a menu bar
+    // hInstance: the first parameter from WinMain
+    // NULL: not used in this application
+    HWND hWnd = CreateWindowEx(
+        WS_EX_OVERLAPPEDWINDOW,
+        szWindowClass,
+        szTitle,
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        500, 200,
+        NULL,
+        NULL,
+        hInstance,
+        NULL
+    );
+    HWND hBtnWnd1 = CreateWindowEx(0, TEXT("BUTTON"), TEXT("Button1"), WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+        30, 30, 100, 30, hWnd, (HMENU)1002, hInstance, 0);
+
+    if (!hWnd)
+    {
+        MessageBox(NULL,
+            _T("调用窗口创建函数失败"),
+            (NULL),
+            NULL);
+
+        return 1;
+    }
+
+    // The parameters to ShowWindow explained:
+    // hWnd: the value returned from CreateWindow
+    // nCmdShow: the fourth parameter from WinMain
+    ShowWindow(hWnd,
+        nCmdShow);
+    UpdateWindow(hWnd);
+
+    // Main message loop:
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
+}
+
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE:  Processes messages for the main window.
+//
+//  WM_PAINT    - Paint the main window
+//  WM_DESTROY  - post a quit message and return
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    PAINTSTRUCT ps;
+    HDC hdc;
+    TCHAR greeting[] = _T("Hello, Windows desktop!");
+
+    switch (message)
+    {
+    case WM_PAINT:
+        hdc = BeginPaint(hWnd, &ps);
+
+        // Here your application is laid out.
+        // For this introduction, we just print out "Hello, Windows desktop!"
+        // in the top left corner.
+        TextOut(hdc,
+            5, 5,
+            greeting, _tcslen(greeting));
+        // End application-specific layout section.
+
+        EndPaint(hWnd, &ps);
+        break;
+    case WM_COMMAND: {
+        int wmId = LOWORD(wParam);
+        // 处理按钮点击事件
+        char wmidstr = char(wmId);
+        if (wmId == 1002) {
+            MessageBoxA(NULL, "按钮被点击了！", "提示", MB_ICONINFORMATION);
+        }
+        break;
+    }
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+        break;
+    }
+
+    return 0;
+}
