f14589ba058ac6db2649fc6f1595a919b9203701
yj94 <2991645986@qq.com>
2024-01-16 22:39:30 +0800

    update

--- Week4/STL_learn/vector_useage.cpp
+++ Week4/STL_learn/vector_useage.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int vec() {
 	int arr[] = { 1,2,3,4,5 };
 	vector<int> v1( arr,arr + 5 );//数组指针赋值 左闭右开 区间存储v1
 	vector<int> v2( 3,5 );//存储3个5
@@ -52,5 +52,48 @@ int main() {
 		cout<< "访问 " << vId[i] << " ";//若越界则直接退出
 	}
 	cout << endl;
+	return 0;
+}
+int vec_it() {
+	//迭代器的使用
+	int arr[] = { 1,2,3,4,5 };
+	vector<int>vIa, vIb, vIc, vId;
+	vIa.assign(arr,arr+5);
+	vector<int>::iterator ita;
+	for (ita = vIa.begin(); ita != vIa.end(); ita++) {
+		cout << *ita << " ";
+	}
+	cout<< endl;
+
+	//空指针问题 迭代器失效
+	vector<int>::iterator itb=vIa.begin()+3;
+	cout <<"迭代器vIa+3 " << *itb << endl;
+	//插入后使用迭代器出现空指针
+	vIa.insert(itb, 8);
+	//应该重新赋值迭代器
+	itb = vIa.begin() + 3;
+	cout<<"迭代器重新赋值后vIa+3 " << *itb << endl;
+	
+	vIb = { 1,2,3,3,3,3,4,5 };
+	vector<int>::iterator itc;
+	for (itc = vIb.begin(); itc != vIb.end();) { //若自增放在for循环中 会导致访问到被erase的元素的空指针
+		if (*itc == 3) {
+			itc = vIb.erase(itc);
+		}
+		else {
+			cout << *itc << endl;
+			++itc;
+		}
+	}
+	//或者使用新迭代器存储erase后的容器 可能会因为编译器认定迭代器失效导致新迭代器失效
+	//或者将指针后移
+	for (itc = vIb.begin(); itc != vIb.end();itc++) { //若自增放在for循环中 会导致访问到被erase的元素的空指针
+		if (*itc == 3) {
+			itc = vIb.erase(itc);
+			itc--;
+		}
+		cout << *itc << endl;
+	}
+
 	return 0;
 }
